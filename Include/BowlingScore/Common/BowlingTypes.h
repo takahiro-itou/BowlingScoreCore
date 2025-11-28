8e9c24b5483e79f8e922db38919f3ebc2733136b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-28 20:51:27 +0900

    型宣言を追加

--- Include/BowlingScore/Common/BowlingTypes.h
+++ Include/BowlingScore/Common/BowlingTypes.h
@@ -33,6 +33,66 @@
 
 BOWLINGSCORE_NAMESPACE_BEGIN
 
+//========================================================================
+//
+//    Type Definitions.
+//
+
+//----------------------------------------------------------------
+/**
+**    エラーコード。
+**/
+
+enum class  ErrCode
+{
+    /**   正常終了。    **/
+    SUCCESS                 = 0,
+
+    /**   異常終了。エラーの理由は不明または報告なし。  **/
+    FAILURE                 = 1,
+
+    /**   無効なインデックス指定。  **/
+    INDEX_OUT_OF_RANGE      = 2,
+
+    /**   ファイルオープンエラー。  **/
+    FILE_OPEN_ERROR         = 3,
+
+    /**   ファイル入出力エラー。    **/
+    FILE_IO_ERROR           = 4,
+
+    /**   不正なファイル形式。      **/
+    FILE_INVALID_FORMAT     = 5,
+};
+
+//----------------------------------------------------------------
+/**
+**    型安全なブール型。
+**/
+
+enum  Boolean
+{
+    BOOL_FALSE  =  0,       /**<  偽。  **/
+    BOOL_TRUE   =  1        /**<  真。  **/
+};
+
+
+//========================================================================
+//
+//    安全なポインタ型のキャスト。
+//
+
+template  <typename  T>
+T  pointer_cast(void  *  p)
+{
+    return ( static_cast<T>(p) );
+}
+
+template  <typename  T>
+T  pointer_cast(const  void  *  p)
+{
+    return ( static_cast<T>(p) );
+}
+
 
 BOWLINGSCORE_NAMESPACE_END
 
